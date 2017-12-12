7aae94818680481870a17828409a489eaa5faae7
dingming <740148050@qq.com>
2017-12-12 13:49:50 +0800

    feat: webpack is good

--- dist/js/a-515d74e73ef602864930.js
+++ dist/js/a-515d74e73ef602864930.js
@@ -57,7 +57,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "http://cdn.com/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 1);
@@ -69,7 +69,7 @@
 /***/ (function(module, exports) {
 
 function a(){
-    
+    console.log(11)
 }
 
 /***/ })
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = { //common js 规范
     // entry: './src/script/main.js', //打包入口文件
     // entry: ['./src/script/main.js', './src/script/a.js'],//为了能打包多个平行文件，可以写成数组的形式
@@ -6,7 +8,23 @@ module.exports = { //common js 规范
         a : "./src/script/a.js"
     },
     output: {  //输出文件
-        path: __dirname + "/dist/js",// 打包后的路径
-        filename: "[name]-[chunkhash].js" // 打包后的名字
-    }
+        path: __dirname + "/dist",// 打包后的路径
+        filename: "js/[name]-[chunkhash].js", // 打包后的名字
+        publicPath: 'http://cdn.com/',
+    },
+    plugins: [ //插件
+        new htmlWebpackPlugin({
+            // filename : 'index-[hash].html', //能设置hsah值
+            filename : 'index.html', 
+            template : 'index.html',
+            // inject : 'head',
+            inject : false,
+            title : 'webpack is good',
+            data : new Date(),
+            minify: { //压缩
+                removeComments: true,
+                collapseWhitespace: true
+            }
+        })
+    ]
 }
