ccaac5b6b4893929d81c385412e5c1fb51cebd77
dingming <740148050@qq.com>
2017-12-15 16:55:04 +0800

    feat: add plugins use

---
+++ README.md
@@ -0,0 +1,9 @@
+# webpack Demo
+
+## webpack 基础的一些配置，
+
+## entry 分文件引入  output 分别打包
+
+### plugins => html-webpack-plugin的使用
+
+### 各种基础的loader包的使用和基础的用法
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,37 @@
 var htmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path')
 
+
+// module.exports = { //common js 规范
+//     // entry: './src/script/main.js', //打包入口文件  ------> 分文件打包
+//     // entry: ['./src/script/main.js', './src/script/a.js'],//为了能打包多个平行文件，可以写成数组的形式
+//     entry: {  //对象的形式 输出文件
+//         main : "./src/script/main.js",
+//         a : "./src/script/a.js"
+//     },
+//     output: {  //输出文件
+//         path: __dirname + "/dist/js",// 打包后的路径
+//         filename: "[name]-[chunkhash].js" // 打包后的名字
+//     }
+// }
+
+// plugins: [ //插件   htmlWebpackPlugin-----配置
+//     new htmlWebpackPlugin({
+//         // filename : 'index-[hash].html', //能设置hsah值
+//         filename : 'index.html', 
+//         template : 'index.html',
+//         // inject : 'head',
+//         inject : false,
+//         title : 'webpack is good',
+//         data : new Date(),
+//         minify: { //压缩
+//             removeComments: true,
+//             collapseWhitespace: true
+//         }
+//     })
+// ]
+
+
 module.exports = {
     entry: './src/app.js',
     output: {
@@ -98,3 +129,4 @@ module.exports = {
         ]
     },
 }
+
